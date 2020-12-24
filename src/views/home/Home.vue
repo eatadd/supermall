be0d0cdffb8449448a393561308e13e6ee316361
eatadd <hello15427@gmail.com>
2020-12-24 08:34:12 +0800

    Merge branch 'main' of https://github.com/eatadd/vue into main
    # Conflicts:
    #	README.md

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,29 +1,34 @@
-<template>
-  <nav-bar class="home-nav">
-    <template v-slot:center>
-      <div>购物街</div>
-    </template>
-  </nav-bar>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <div>
+    <nav-bar class="home-nav">
+      <template v-slot:center>
+        <div>首页</div>
+      </template>
+    </nav-bar>
+    <swiper :banners="banners"></swiper>
+  </div>
 </template>
 
 <script>
   import NavBar from '@/components/common/navbar/NavBar'
   import {getHomeMultidata} from "@/network/home";
+  import Swiper from '@/components/common/swiper/Swpier'
 
   export default {
     name: "Home",
     components: {
-      NavBar
+      NavBar,
+      Swiper
     },
-    data(){
+    data() {
       return {
-        banners: [],
-        recommend:[]
+        banners: null,
+        recommend: null,
       }
     },
+    methods: {},
     created() {
-      getHomeMultidata().then(res=>{
-        console.log(res);
+      getHomeMultidata().then(res => {
         this.banners = res.data.banner.list;
         this.recommend = res.data.recommend.list;
       })
